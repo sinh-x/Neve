2851eed7d0e8de30c14c0f7b423b5ea402f6129e
sinh-x <sinh@sinhn.com>
2024-07-18 21:19:57 +0700

    Add obsidian

---
+++ config/utils/obsidian.nix
@@ -0,0 +1,11 @@
+{
+  plugins.obsidian = {
+    enable = true;
+    settings = {
+      workspaces = {
+        name = "personal";
+        path = "~/Documents/Sinh-organized";
+      };
+    };
+  };
+}
