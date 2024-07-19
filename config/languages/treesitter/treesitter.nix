00a232431ce8946814a9b946ec891f049227d8ec
sinh-x <sinh@sinhn.com>
2024-07-19 11:10:25 +0700

    Update treesitter keymap

--- config/languages/treesitter/treesitter.nix
+++ config/languages/treesitter/treesitter.nix
@@ -1,8 +1,22 @@
-{
+{pkgs, ...}: {
   plugins.treesitter = {
     enable = true;
     indent = true;
     folding = true;
     nixvimInjections = true;
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
   };
+  extraConfigLua = ''
+    require("nvim-treesitter.configs").setup({
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  '';
 }
