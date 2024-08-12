525e6618d7fb998f470c4a97593ff8199f024541
Sinh Nguyen <sinh@sinhn.com>
2024-08-12 23:36:16 +0700

    Adjust gitsign

--- packages/sinh-x-nixvim/plugins/git/gitsigns.nix
+++ packages/sinh-x-nixvim/plugins/git/gitsigns.nix
@@ -1,70 +1,145 @@
-_: {
-  plugins.gitsigns = {
-    enable = true;
-    settings = {
-      trouble = true;
-      current_line_blame = false;
+{ config, lib, ... }:
+let
+  inherit (builtins) toJSON;
+  inherit (lib) mkIf;
+in
+{
+
+  plugins = {
+    gitsigns = {
+      enable = true;
+
+      settings = {
+        current_line_blame = true;
+
+        current_line_blame_opts = {
+          delay = 500;
+
+          ignore_blank_lines = true;
+          ignore_whitespace = true;
+          virt_text = true;
+          virt_text_pos = "eol";
+        };
+
+        signcolumn = false;
+      };
     };
+
+    which-key.settings.spec = lib.optionals config.plugins.gitsigns.enable [
+      {
+        __unkeyed = "<leader>gh";
+        group = "  Hunks";
+      }
+      {
+        __unkeyed = "<leader>ug";
+        group = "󰊢 Git";
+      }
+    ];
   };
-  keymaps = [
+
+  keymaps = mkIf config.plugins.gitsigns.enable [
+    # UI binds
     {
-      mode = [
-        "n"
-        "v"
-      ];
-      key = "<leader>gh";
-      action = "gitsigns";
+      mode = "n";
+      key = "<leader>ugb";
+      action = ":Gitsigns toggle_current_line_blame<CR>";
       options = {
+        desc = "Git Blame toggle";
         silent = true;
-        desc = "+hunks";
       };
     }
     {
       mode = "n";
-      key = "<leader>ghb";
-      action = ":Gitsigns blame_line<CR>";
+      key = "<leader>ugd";
+      action = ":Gitsigns toggle_deleted<CR>";
       options = {
+        desc = "Deleted toggle";
         silent = true;
-        desc = "Blame line";
       };
     }
     {
       mode = "n";
-      key = "<leader>ghd";
-      action = ":Gitsigns diffthis<CR>";
+      key = "<leader>ugl";
+      action = ":Gitsigns toggle_linehl<CR>";
       options = {
+        desc = "Line Highlight toggle";
         silent = true;
-        desc = "Diff This";
       };
     }
+    {
+      mode = "n";
+      key = "<leader>ugh";
+      action = ":Gitsigns toggle_numhl<CR>";
+      options = {
+        desc = "Number Highlight toggle";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ugw";
+      action = "<cmd>Gitsigns toggle_word_diff<CR>";
+      options = {
+        desc = "Word Diff toggle";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ugs";
+      action = "<cmd>Gitsigns toggle_signs<CR>";
+      options = {
+        desc = "Signs toggle";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gb";
+      action.__raw = # Lua
+        ''
+          function() require("gitsigns").blame_line{full=true} end
+        '';
+      options = {
+        desc = "Git Blame toggle";
+        silent = true;
+      };
+    }
+    # Hunk binds
     {
       mode = "n";
       key = "<leader>ghp";
-      action = ":Gitsigns preview_hunk<CR>";
+      action.__raw = # Lua
+        ''
+          function()
+            if vim.wo.diff then return ${toJSON "<leader>gp"} end
+
+            vim.schedule(function() require("gitsigns").prev_hunk() end)
+
+            return '<Ignore>'
+          end
+        '';
       options = {
+        desc = "Previous hunk";
         silent = true;
-        desc = "Preview hunk";
       };
     }
     {
       mode = "n";
-      key = "<leader>ghR";
-      action = ":Gitsigns reset_buffer<CR>";
+      key = "<leader>ghn";
+      action.__raw = # Lua
+        ''
+          function()
+            if vim.wo.diff then return ${toJSON "<leader>gn"} end
+
+            vim.schedule(function() require("gitsigns").next_hunk() end)
+
+            return '<Ignore>'
+          end
+        '';
       options = {
+        desc = "Next hunk";
         silent = true;
-        desc = "Reset Buffer";
-      };
-    }
-    {
-      mode = [
-        "n"
-        "v"
-      ];
-      key = "<leader>ghr";
-      action = ":Gitsigns reset_hunk<CR>";
-      options = {
-        silent = true;
-        desc = "Reset Hunk";
       };
     }
     {
@@ -73,28 +148,68 @@ _: {
         "v"
       ];
       key = "<leader>ghs";
-      action = ":Gitsigns stage_hunk<CR>";
+      action = "<cmd>Gitsigns stage_hunk<CR>";
       options = {
+        desc = "Stage hunk";
         silent = true;
-        desc = "Stage Hunk";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>ghS";
-      action = ":Gitsigns stage_buffer<CR>";
-      options = {
-        silent = true;
-        desc = "Stage Buffer";
       };
     }
     {
       mode = "n";
       key = "<leader>ghu";
-      action = ":Gitsigns undo_stage_hunk<CR>";
+      action = "<cmd>Gitsigns undo_stage_hunk<CR>";
       options = {
+        desc = "Undo stage hunk";
         silent = true;
-        desc = "Undo Stage Hunk";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>ghr";
+      action = "<cmd>Gitsigns reset_hunk<CR>";
+      options = {
+        desc = "Reset hunk";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ghP";
+      action = "<cmd>Gitsigns preview_hunk<CR>";
+      options = {
+        desc = "Preview hunk";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gh<C-p>";
+      action = "<cmd>Gitsigns preview_hunk_inline<CR>";
+      options = {
+        desc = "Preview hunk inline";
+        silent = true;
+      };
+    }
+    # Buffer binds
+    {
+      mode = "n";
+      key = "<leader>gS";
+      action = "<cmd>Gitsigns stage_buffer<CR>";
+      options = {
+        desc = "Stage buffer";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gR";
+      action = "<cmd>Gitsigns reset_buffer<CR>";
+      options = {
+        desc = "Reset buffer";
+        silent = true;
       };
     }
   ];
