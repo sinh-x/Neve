c6c17e847371a594e34550cf22d8fd37947e6cd4
Sinh Nguyen <sinh@sinhn.com>
2024-08-12 23:36:07 +0700

    Update harpoon

--- packages/sinh-x-nixvim/plugins/utils/harpoon.nix
+++ packages/sinh-x-nixvim/plugins/utils/harpoon.nix
@@ -1,56 +1,58 @@
 { lib, config, ... }:
 {
-  plugins.harpoon = {
-    enable = true;
-    enableTelescope = true;
-    keymapsSilent = true;
-    menu = {
-      width = 120;
-    };
-    keymaps = {
-      addFile = "<leader>ha";
-      toggleQuickMenu = "<leader>he";
-      navFile = {
-        "1" = "<leader>h1";
-        "2" = "<leader>h2";
-        "3" = "<leader>h3";
-        "4" = "<leader>h4";
-        "5" = "<leader>h5";
+  plugins = {
+    harpoon = {
+      enable = true;
+      enableTelescope = true;
+      keymapsSilent = true;
+      menu = {
+        width = 120;
+      };
+      keymaps = {
+        addFile = "<leader>ha";
+        toggleQuickMenu = "<leader>he";
+        navFile = {
+          "1" = "<leader>h1";
+          "2" = "<leader>h2";
+          "3" = "<leader>h3";
+          "4" = "<leader>h4";
+          "5" = "<leader>h5";
+        };
       };
     };
+    which-key.settings.spec = lib.optionals config.plugins.harpoon.enable [
+      {
+        __unkeyed = "<leader>h";
+        group = "󱡀 Harpoon";
+      }
+      {
+        __unkeyed = "<leader>ha";
+        desc = "Add";
+      }
+      {
+        __unkeyed = "<leader>he";
+        desc = "QuickMenu";
+      }
+      {
+        __unkeyed = "<leader>h1";
+        desc = "1";
+      }
+      {
+        __unkeyed = "<leader>h2";
+        desc = "2";
+      }
+      {
+        __unkeyed = "<leader>h3";
+        desc = "3";
+      }
+      {
+        __unkeyed = "<leader>h4";
+        desc = "4";
+      }
+      {
+        __unkeyed = "<leader>h5";
+        desc = "5";
+      }
+    ];
   };
-  which-key.settings.spec = lib.optionals config.plugins.harpoon.enable [
-    {
-      __unkeyed = "<leader>h";
-      group = "󱡀 Harpoon";
-    }
-    {
-      __unkeyed = "<leader>ha";
-      desc = "Add";
-    }
-    {
-      __unkeyed = "<leader>he";
-      desc = "QuickMenu";
-    }
-    {
-      __unkeyed = "<leader>h1";
-      desc = "1";
-    }
-    {
-      __unkeyed = "<leader>h2";
-      desc = "2";
-    }
-    {
-      __unkeyed = "<leader>h3";
-      desc = "3";
-    }
-    {
-      __unkeyed = "<leader>h4";
-      desc = "4";
-    }
-    {
-      __unkeyed = "<leader>h5";
-      desc = "5";
-    }
-  ];
 }
