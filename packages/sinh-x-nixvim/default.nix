7f78307b9df43fb0d3761470213a1405d43e32e6
sinh-x <sinh@sinhn.com>
2024-08-16 19:31:59 +0700

    Update theme

--- packages/sinh-x-nixvim/default.nix
+++ packages/sinh-x-nixvim/default.nix
@@ -4,21 +4,54 @@
   system,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (inputs) nixvim;
 in
-  nixvim.legacyPackages.${system}.makeNixvimWithModule {
-    inherit pkgs;
+nixvim.legacyPackages.${system}.makeNixvimWithModule {
+  inherit pkgs;
 
-    module = {
-      imports = lib.snowfall.fs.get-non-default-nix-files-recursive ./.;
+  module = {
+    imports = lib.snowfall.fs.get-non-default-nix-files-recursive ./.;
 
-      luaLoader.enable = true;
+    luaLoader.enable = true;
 
-      # Highlight and remove extra white spaces
-      highlight.ExtraWhitespace.bg = "red";
-      match.ExtraWhitespace = "\\s\\+$";
+    # Highlight and remove extra white spaces
+    highlight.ExtraWhitespace.bg = "red";
+    match.ExtraWhitespace = "\\s\\+$";
 
-      colorschemes.catppuccin.enable = true;
+    colorschemes.tokyonight = {
+      enable = true;
+      settings = {
+        day_brightness = 0.3;
+        dim_inactive = false;
+        hide_inactive_statusline = false;
+        light_style = "day";
+        lualine_bold = false;
+        on_colors = "function(colors) end";
+        on_highlights = "function(highlights, colors) end";
+        sidebars = [
+          "qf"
+          "vista_kind"
+          "terminal"
+          "packer"
+        ];
+        style = "storm";
+        styles = {
+          comments = {
+            italic = true;
+          };
+          floats = "dark";
+          functions = { };
+          keywords = {
+            italic = true;
+          };
+          sidebars = "dark";
+          variables = { };
+        };
+        terminal_colors = true;
+        transparent = true;
+      };
     };
-  }
+  };
+}
