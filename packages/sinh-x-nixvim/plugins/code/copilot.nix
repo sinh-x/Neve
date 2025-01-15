67a416beae40505e04fe3f3d053f7597ba8ce23a
sinh-x <sinh@sinhn.com>
2025-01-15 15:55:29 +0700

    Nixvim update flake

--- packages/sinh-x-nixvim/plugins/code/copilot.nix
+++ packages/sinh-x-nixvim/plugins/code/copilot.nix
@@ -1,47 +1,49 @@
 {
   plugins.copilot-lua = {
     enable = true;
-    panel = {
-      enabled = false;
-      autoRefresh = true;
-      keymap = {
-        jumpPrev = "[[";
-        jumpNext = "]]";
-        accept = "<CR>";
-        refresh = "gr";
-        open = "<M-CR>";
+    settings = {
+      panel = {
+        enabled = false;
+        autoRefresh = true;
+        keymap = {
+          jumpPrev = "[[";
+          jumpNext = "]]";
+          accept = "<CR>";
+          refresh = "gr";
+          open = "<M-CR>";
+        };
+        layout = {
+          position = "bottom"; # | top | left | right
+          ratio = 0.4;
+        };
       };
-      layout = {
-        position = "bottom"; # | top | left | right
-        ratio = 0.4;
+      suggestion = {
+        enabled = false;
+        autoTrigger = true;
+        debounce = 75;
+        keymap = {
+          accept = "<M-l>";
+          acceptWord = false;
+          acceptLine = false;
+          next = "<M-]>";
+          prev = "<M-[>";
+          dismiss = "<C-]>";
+        };
       };
-    };
-    suggestion = {
-      enabled = false;
-      autoTrigger = true;
-      debounce = 75;
-      keymap = {
-        accept = "<M-l>";
-        acceptWord = false;
-        acceptLine = false;
-        next = "<M-]>";
-        prev = "<M-[>";
-        dismiss = "<C-]>";
+      filetypes = {
+        yaml = false;
+        markdown = false;
+        help = false;
+        gitcommit = false;
+        gitrebase = false;
+        hgcommit = false;
+        svn = false;
+        cvs = false;
+        "." = false;
       };
+      copilotNodeCommand = "node"; # Node.js version must be > 18.x
+      serverOptsOverrides = { };
     };
-    filetypes = {
-      yaml = false;
-      markdown = false;
-      help = false;
-      gitcommit = false;
-      gitrebase = false;
-      hgcommit = false;
-      svn = false;
-      cvs = false;
-      "." = false;
-    };
-    copilotNodeCommand = "node"; # Node.js version must be > 18.x
-    serverOptsOverrides = { };
   };
   plugins.copilot-chat.enable = true;
 }
