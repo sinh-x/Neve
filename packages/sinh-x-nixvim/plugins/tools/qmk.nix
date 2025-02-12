5bf4586499f1b4003a6a5ba2a0cf7c32340578ad
sinh-x <sinh@sinhn.com>
2025-02-12 21:51:22 +0700

    Use nixvim qmk instead of git-repo build

--- packages/sinh-x-nixvim/plugins/tools/qmk.nix
+++ packages/sinh-x-nixvim/plugins/tools/qmk.nix
@@ -1,30 +1,46 @@
-_: {
-  # extraPlugins = with pkgs.vimUtils; [
-  #   (buildVimPlugin {
-  #     pname = "qmk.nvim";
-  #     version = "2024-09-14";
-  #     src = pkgs.fetchFromGitHub {
-  #       owner = "codethread";
-  #       repo = "qmk.nvim";
-  #       rev = "ad51cb15e607da0983fcf9882d38a2aafac32149";
-  #       sha256 = "sha256-uNLfrk53JStVZdT42brqR1I4swU/rG6gZQbsDk+vaJE=";
-  #     };
-  #   })
-  # ];
-  # extraConfigLua = ''
-  #   local qmk = require("qmk")
-  #   qmk.setup({
-  #     name = "LAYOUT_sinh_x_58",
-  #     variant = "qmk",
-  #     layout = {
-  #       "_ x x x x x x _ _ _ x x x x x x",
-  #       "_ x x x x x x _ _ _ x x x x x x",
-  #       "_ x x x x x x _ _ _ x x x x x x",
-  #       "_ x x x x x x x _ x x x x x x x",
-  #       "_ _ _ x x x x x _ x x x x x _ _",
-  #     },
-  #   })
-  # '';
-  #
-  # keymaps = [ ];
+{
+  plugins.qmk = {
+    enable = true;
+    settings = {
+      name = "LAYOUT_sinh_x_58";
+      variant = "qmk";
+      layout = [
+        "_ x x x x x x _ _ _ x x x x x x"
+        "_ x x x x x x _ _ _ x x x x x x"
+        "_ x x x x x x _ _ _ x x x x x x"
+        "_ x x x x x x x _ x x x x x x x"
+        "_ _ _ x x x x x _ x x x x x _ _"
+      ];
+    };
+  };
 }
+# _: {
+# extraPlugins = with pkgs.vimUtils; [
+#   (buildVimPlugin {
+#     pname = "qmk.nvim";
+#     version = "2024-09-14";
+#     src = pkgs.fetchFromGitHub {
+#       owner = "codethread";
+#       repo = "qmk.nvim";
+#       rev = "ad51cb15e607da0983fcf9882d38a2aafac32149";
+#       sha256 = "sha256-uNLfrk53JStVZdT42brqR1I4swU/rG6gZQbsDk+vaJE=";
+#     };
+#   })
+# ];
+# extraConfigLua = ''
+#   local qmk = require("qmk")
+#   qmk.setup({
+#     name = "LAYOUT_sinh_x_58",
+#     variant = "qmk",
+#     layout = {
+#       "_ x x x x x x _ _ _ x x x x x x",
+#       "_ x x x x x x _ _ _ x x x x x x",
+#       "_ x x x x x x _ _ _ x x x x x x",
+#       "_ x x x x x x x _ x x x x x x x",
+#       "_ _ _ x x x x x _ x x x x x _ _",
+#     },
+#   })
+# '';
+#
+# keymaps = [ ];
+# }
